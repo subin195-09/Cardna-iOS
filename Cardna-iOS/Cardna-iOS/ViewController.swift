0b12b33252d2ea1adf0cd96653060a55e76ad2cc
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-09 15:14:34 +0900

    [#2] podfile 추가

---
+++ Cardna-iOS/Cardna-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cardna-iOS
+//
+//  Created by 김혜수 on 2022/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
