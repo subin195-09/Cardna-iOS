2e76e1829397b9d40cd9b45070db8c164bd9c679
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-09 16:10:29 +0900

    [#2] icon asset 추가

---
+++ Cardna-iOS/Cardna-iOS/Configuration/Constants/Const.swift
@@ -0,0 +1,12 @@
+//
+//  Const.swift
+//  Cardna-iOS
+//
+//  Created by 김혜수 on 2022/01/09.
+//
+
+import Foundation
+
+struct Const {
+    // 구체적인 상수는 Constants 그룹 내부 파일로 각각 Extension 에 작성
+}
---
+++ Cardna-iOS/Cardna-iOS/Configuration/Constants/Image.swift
@@ -0,0 +1,42 @@
+//
+//  Image.swift
+//  Cardna-iOS
+//
+//  Created by 김혜수 on 2022/01/09.
+//
+
+import UIKit
+
+extension Const {
+    
+    struct Image {
+        
+        // MARK: - icon Tabbar
+        
+        static let homeTabIcon = UIImage(named: "btmnavi1Inactive")!
+        static let homeTabIconActive = UIImage(named: "btmnavi1Active")!
+        static let cardPackTabIcon = UIImage(named: "btmnavi2Inactive")!
+        static let cardPackTabIconActive = UIImage(named: "btmnavi2Active")!
+        static let insightTabIcon = UIImage(named: "btmnavi3Inactive")!
+        static let insightTabIconActive = UIImage(named: "btmnavi3Active")!
+        static let myPageTabIcon = UIImage(named: "btmnavi4Inactive")!
+        static let myPageTabIconActive = UIImage(named: "btmnavi4Active")!
+        
+        // MARK: - icon Navigationbar
+        
+        static let searchNaviIcon = UIImage(named: "icbtSearch")!
+        static let cardPlusNaviIcon = UIImage(named: "icbtCardplus")!
+        
+        // MARK: - 카드추가 모달 icon
+        
+        static let cardmeIcon = UIImage(named: "icbtCardme")!
+        static let cardyouIcon = UIImage(named: "icbtCardyou")!
+        
+        // MARK: - 카드팩 icon
+        
+        static let cardPackMeIconActive = UIImage(named: "icbtCardmeAcrive")!
+        //static let cardPackYouIconActive = UIImage(named: "")
+        //static let cardPackMeIcon
+        static let cardPackYouIcon = UIImage(named: "icbtCardyouInacrive")!
+    }
+}
