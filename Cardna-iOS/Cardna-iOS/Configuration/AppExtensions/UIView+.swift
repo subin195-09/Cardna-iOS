2e26793f1de586a11d17017f66781f42e36442c7
meenyweeny <kmeye@gmail.com>
2022-01-11 19:16:59 +0900

    [#9] extension 추가

--- Cardna-iOS/Cardna-iOS/Configuration/AppExtensions/UIView+.swift
+++ Cardna-iOS/Cardna-iOS/Configuration/AppExtensions/UIView+.swift
@@ -5,4 +5,13 @@
 //  Created by 이경민 on 2022/01/11.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func addSubviews(_ views: UIView...) {
+        for view in views {
+            self.addSubview(view)
+        }
+    }
+}
