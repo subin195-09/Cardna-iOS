78292e195a104f71d381d8f5d4ba789c0b50d177
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-11 18:03:38 +0900

    [#5] 카드셀 UI 만들기

---
+++ Cardna-iOS/Cardna-iOS/Source/AppSources/AppXibs/CardCollectionViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  CardCollectionViewCell.swift
+//  Cardna-iOS
+//
+//  Created by 김혜수 on 2022/01/11.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Property
+    
+    public static let identifier = "CardCollectionViewCell"
+
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // MARK: - LifeCycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUI()
+    }
+    
+    // MARK: - Function
+    
+    func setUI() {
+        bgView.layer.cornerRadius = 8
+        bgView.backgroundColor = .mainGreen
+        titleLabel.font = .cardnaB4Sbd
+    }
+    
+    func setData(image: UIImage, title: String, isMe: Bool) {
+        imageView.image = image
+        titleLabel.text = title
+        if isMe == true {
+            bgView.backgroundColor = .mainGreen
+        }
+        else {
+            bgView.backgroundColor = .mainPurple
+        }
+    }
+
+}
