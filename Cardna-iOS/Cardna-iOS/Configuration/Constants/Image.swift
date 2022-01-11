5f28b9eabfd6b1ebce2398df7b87d55af51ae9d6
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-12 03:43:40 +0900

    [#12] 2차 아이콘 추가

--- Cardna-iOS/Cardna-iOS/Configuration/Constants/Image.swift
+++ Cardna-iOS/Cardna-iOS/Configuration/Constants/Image.swift
@@ -11,6 +11,9 @@ extension Const {
     
     struct Image {
         
+        // MARK: - Dummy Data Image (추후삭제)
+        
+        
         // MARK: - icon Tabbar
         
         static let homeTabIcon = UIImage(named: "btmnavi1Inactive")!
@@ -24,19 +27,59 @@ extension Const {
         
         // MARK: - icon Navigationbar
         
-        static let searchNaviIcon = UIImage(named: "icbtSearch")!
-        static let cardPlusNaviIcon = UIImage(named: "icbtCardplus")!
+        static let icbtSearch = UIImage(named: "icbtSearch")!
+        static let icbtCardplus = UIImage(named: "icbtCardplus")!
+        static let icbtBack = UIImage(named: "icbtBack")!
+        static let icbtNotification = UIImage(named: "icbtNotification")!
+        static let icbtShare = UIImage(named: "icbtShare")!
+        static let icbtSeemore = UIImage(named: "icbtSeemore")!
+        static let icbtBoxbackPurple = UIImage(named: "icbtBoxbackPurple")!
+        static let icbtBoxbackGreen = UIImage(named: "icbtBoxbackGreen")!
+        static let icbtBoxbackGray = UIImage(named: "icbtBoxbackGray")!
+        
         
         // MARK: - 카드추가 모달 icon
         
-        static let cardmeIcon = UIImage(named: "icbtCardme")!
-        static let cardyouIcon = UIImage(named: "icbtCardyou")!
+        static let icbtCardme = UIImage(named: "icbtCardme")!
+        static let icbtCardyou = UIImage(named: "icbtCardyou")!
         
         // MARK: - 카드팩 icon
         
-        static let cardPackMeIconActive = UIImage(named: "icbtCardmeActive")!
-        static let cardPackYouIconActive = UIImage(named: "icbtCardyouActive")!
-        static let cardPackMeIcon = UIImage(named: "icbtCardmeInactive")!
-        static let cardPackYouIcon = UIImage(named: "icbtCardyouInactive")!
+        static let icbtCardmeActive = UIImage(named: "icbtCardmeActive")!
+        static let icbtCardyouActive = UIImage(named: "icbtCardyouActive")!
+        static let icbtCardmeInactive = UIImage(named: "icbtCardmeInactive")!
+        static let icbtCardyouInactive = UIImage(named: "icbtCardyouInactive")!
+        
+        // MARK: - 카드추가뷰 심볼
+        
+        static let icbtSymbolSmile = UIImage(named: "icbtSymbolSmile")!
+        static let icbtSymbolSmileSelect = UIImage(named: "icbtSymbolSmileSelect")!
+        static let icbtSymbolSmilePur = UIImage(named: "icbtSymbolSmilePur")!
+        static let icbtSymbolSmileSelectPur = UIImage(named: "icbtSymbolSmileSelectPur")!
+        
+        static let icbtSymbolHeart = UIImage(named: "icbtSymbolHeart")!
+        static let icbtSymbolHeartSelect = UIImage(named: "icbtSymbolHeartSelect")!
+        static let icbtSymbolHeartPur = UIImage(named: "icbtSymbolHeartPur")!
+        static let icbtSymbolHeartSelectPur = UIImage(named: "icbtSymbolHeartSelectPur")!
+        
+        static let icbtSymbolDia = UIImage(named: "icbtSymbolDia")!
+        static let icbtSymbolDiaSelect = UIImage(named: "icbtSymbolDiaSelect")!
+        static let icbtSymbolDiaPur = UIImage(named: "icbtSymbolDiaPur")!
+        static let icbtSymbolDiaSelectPur = UIImage(named: "icbtSymbolDiaSelectPur")!
+        
+        static let icbtSymbolSpade = UIImage(named: "icbtSymbolSpade")!
+        static let icbtSymbolSpadeSelect = UIImage(named: "icbtSymbolSpadeSelect")!
+        static let icbtSymbolSpadePur = UIImage(named: "icbtSymbolSpadePur")!
+        static let icbtSymbolSpadeSelectPur = UIImage(named: "icbtSymbolSpadeSelectPur")!
+        
+        static let icbtSymbolClover = UIImage(named: "icbtSymbolClover")!
+        static let icbtSymbolCloverSelect = UIImage(named: "icbtSymbolCloverSelect")!
+        static let icbtSymbolCloverPur = UIImage(named: "icbtSymbolCloverPur")!
+        static let icbtSymbolCloverSelectPur = UIImage(named: "icbtSymbolCloverSelectPur")!
+        
+        // MARK: - 카드추가뷰 이미지
+        
+        static let imgCardimg = UIImage(named: "imgCardimg")!
     }
+    
 }
