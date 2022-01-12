2d8a9eaf93dd3f1b18835d6e2a24d491a017f2c8
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-12 16:19:59 +0900

    [#13] 홈 대표카드 뷰 셀 추가

--- Cardna-iOS/Cardna-iOS/Source/Scene/Home/HomeViewController.swift
+++ Cardna-iOS/Cardna-iOS/Source/Scene/Home/HomeViewController.swift
@@ -8,21 +8,37 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    // MARK: - Property
+    
     // MARK: - IBOutlet
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: - VC LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCollectionView()
+        registerXib()
         setUI()
     }
     
     // MARK: - Function
-
+    
+    func setCollectionView() {
+        collectionView.backgroundColor = .none
+        collectionView.setCollectionViewLayout(createLayout(), animated: true)
+        collectionView.dataSource = self
+    }
+    
+    func registerXib() {
+        let nib = UINib(nibName: CardCollectionViewCell.identifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: CardCollectionViewCell.identifier)
+    }
+    
     func setUI() {
         setNavigationBarUI()
         setTitleUI()
