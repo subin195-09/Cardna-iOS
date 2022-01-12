513d3ce3c775ea4086af2a1d85664e99a17c325b
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-12 14:49:23 +0900

    [#13] 홈 대표카드 상단 Ui (네비바, 코멘트)

--- Cardna-iOS/Cardna-iOS/Source/Scene/Home/HomeViewController.swift
+++ Cardna-iOS/Cardna-iOS/Source/Scene/Home/HomeViewController.swift
@@ -9,21 +9,31 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    // MARK: - VC LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
+    // MARK: - Function
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUI() {
+        setNavigationBarUI()
+        setTitleUI()
     }
-    */
-
+    
+    func setNavigationBarUI() {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    func setTitleUI() {
+        nameLabel.font = .Pretendard(.light, size: 28)
+        commentLabel.font = .cardnaH1Sbd
+    }
 }
