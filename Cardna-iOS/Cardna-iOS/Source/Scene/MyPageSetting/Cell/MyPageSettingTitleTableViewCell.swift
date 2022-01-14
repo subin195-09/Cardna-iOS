e590b6aa4f2abbf591983acc3951dc09ac908a7f
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-01-15 05:18:28 +0900

    [#32] MyPage 설정 뷰 UI 구현완

--- Cardna-iOS/Cardna-iOS/Source/Scene/MyPageSetting/Cell/MyPageSettingTitleTableViewCell.swift
+++ Cardna-iOS/Cardna-iOS/Source/Scene/MyPageSetting/Cell/MyPageSettingTitleTableViewCell.swift
@@ -9,15 +9,22 @@ import UIKit
 
 class MyPageSettingTitleTableViewCell: UITableViewCell {
 
+    // MARK: - Property
+    
+    static let identifier = "MyPageSettingTitleTableViewCell"
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var functionLabel: UILabel!
+    
+    // MARK: - Cell Life Cycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
