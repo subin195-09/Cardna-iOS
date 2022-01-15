1261eb8b324748520591c27c76dd6026c57fb317
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-15 22:29:01 +0900

    [#44] 마이페이지 메인

--- Cardna-iOS/Cardna-iOS/Source/Scene/MyPage/MyPageViewController.swift
+++ Cardna-iOS/Cardna-iOS/Source/Scene/MyPage/MyPageViewController.swift
@@ -8,22 +8,68 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
-
+    
+    @IBOutlet weak var navigationTitleLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var friendTitleLabel: UILabel!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var friendCountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
+        setTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUI() {
+        setNavigationBarUI()
+        setTableViewHeaderUI()
+        setSearchBarUI()
     }
-    */
-
+    
+    func setNavigationBarUI() {
+        navigationTitleLabel.font = .cardnaH1Sbd
+    }
+    
+    func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        if #available(iOS 15, *) {
+            tableView.sectionFooterHeight = 0
+            tableView.sectionHeaderHeight = 0
+        }
+    }
+    
+    func setTableViewHeaderUI() {
+        friendTitleLabel.textColor = .w1
+        friendTitleLabel.font = .cardnaSh1Sbd
+        friendCountLabel.font = .Pretendard(.regular, size: 16)
+        friendCountLabel.textColor = .w3
+    }
+    
+    func setSearchBarUI(){
+        searchBar.backgroundColor = .clear
+        searchBar.setImage(Const.Image.icbtSearch, for: .search, state: .normal)
+        searchBar.searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.searchTextField.heightAnchor.constraint(equalTo: searchBar.heightAnchor).isActive = true
+        searchBar.searchTextField.widthAnchor.constraint(equalTo: searchBar.widthAnchor).isActive = true
+        
+        searchBar.setBackgroundImage(UIImage(), for: .any, barMetrics: UIBarMetrics.default)
+        if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
+            ///서치바 백그라운드 컬러
+            textfield.backgroundColor = .w4
+            ///서치바 텍스트입력시 색 정하기
+            textfield.textColor = UIColor.w1
+            textfield.placeholder = "이름 검색"
+            ///플레이스홀더 글씨 색 정하기
+            textfield.attributedPlaceholder = NSAttributedString(string: textfield.placeholder ?? "", attributes: [NSAttributedString.Key.foregroundColor : UIColor.w2])
+            textfield.font = .Pretendard(.regular, size: 16)
+            ///왼쪽 아이콘 이미지넣기
+            if let leftView = textfield.leftView as? UIImageView {
+                leftView.image = Const.Image.icbtSearch
+            }
+        }
+    }
+    
 }
