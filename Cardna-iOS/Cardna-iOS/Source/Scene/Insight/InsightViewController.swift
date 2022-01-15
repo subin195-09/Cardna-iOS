1d231ee2cac2bd9c3992846f25c58c813822f16c
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-01-16 07:09:01 +0900

    [#46] 인사이트 뷰 UI 구현 완료

--- Cardna-iOS/Cardna-iOS/Source/Scene/Insight/InsightViewController.swift
+++ Cardna-iOS/Cardna-iOS/Source/Scene/Insight/InsightViewController.swift
@@ -9,21 +9,101 @@ import UIKit
 
 class InsightViewController: UIViewController {
 
+    // MARK: - Property
+    
+    var deviceWidth: CGFloat = UIScreen.main.bounds.width
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageView: UIView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var insightTitleLabel: UILabel!
+    
+    // MARK: Open Area Page outlet
+    @IBOutlet weak var openAreaPageView: UIView!
+    @IBOutlet weak var openAreaTitleLabel: UILabel!
+    @IBOutlet weak var openAreaCardView: UIView!
+    @IBOutlet weak var openAreaCardImageView: UIImageView!
+    @IBOutlet weak var openAreaCardTitleLabel: UILabel!
+    @IBOutlet weak var openAreaExplainLabel: UILabel!
+    
+    // MARK: Blind Area Page outlet
+    @IBOutlet weak var blindAreaPageView: UIView!
+    @IBOutlet weak var blindAreaCardView: UIView!
+    @IBOutlet weak var blindAreaTitleLabel: UILabel!
+    @IBOutlet weak var blindAreaCardImageView: UIImageView!
+    @IBOutlet weak var blindAreaCardTitleLabel: UILabel!
+    @IBOutlet weak var blindAreaExplainLabel: UILabel!
+    
+    // MARK: - VC Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Function
+    
+    private func setUI() {
+        setLabelUI()
+        setCardViewUI()
+        setSize()
     }
-    */
-
+    
+    func setLabelUI() {
+        setLabelFont()
+        setLabelColor()
+    }
+    
+    func setLabelFont() {
+        insightTitleLabel.font = .cardnaH1Sbd
+        [openAreaTitleLabel, blindAreaTitleLabel].forEach {
+            $0?.font = .cardnaB2Sbd
+        }
+        [openAreaCardTitleLabel, blindAreaCardTitleLabel].forEach {
+            $0?.font = .cardnaSh1Sbd
+        }
+        [openAreaExplainLabel, blindAreaExplainLabel].forEach {
+            $0?.font = .cardnaSh3Sbd
+        }
+    }
+    
+    func setLabelColor() {
+        [insightTitleLabel, openAreaTitleLabel, blindAreaTitleLabel, openAreaCardTitleLabel, blindAreaCardTitleLabel, openAreaExplainLabel, blindAreaExplainLabel].forEach {
+            $0?.textColor = UIColor(white: 18.0 / 255.0, alpha: 1.0)
+        }
+    }
+    
+    func setCardViewUI() {
+        openAreaCardView.backgroundColor = .mainGreen
+        blindAreaCardView.backgroundColor = .mainPurple
+        [openAreaCardView, blindAreaCardView].forEach {
+            $0?.layer.cornerRadius = 8.7
+            $0?.layer.borderWidth = 1.7
+            $0?.layer.borderColor = UIColor(white: 18.0 / 255.0, alpha: 1.0).cgColor
+        }
+        [openAreaCardImageView, blindAreaCardImageView].forEach {
+            $0?.layer.borderWidth = 2
+            $0?.layer.borderColor = UIColor(white: 18.0 / 255.0, alpha: 1.0).cgColor
+        }
+    }
+    
+    func setSize() {
+        scrollView.snp.makeConstraints {
+            $0.width.equalTo(deviceWidth*2)
+        }
+        pageView.snp.makeConstraints {
+            $0.width.equalTo(deviceWidth*2)
+        }
+        backgroundImageView.snp.makeConstraints {
+            $0.width.equalTo(deviceWidth*2)
+        }
+        openAreaPageView.snp.makeConstraints {
+            $0.width.equalTo(deviceWidth)
+        }
+        blindAreaPageView.snp.makeConstraints {
+            $0.width.equalTo(deviceWidth)
+        }
+    }
 }
