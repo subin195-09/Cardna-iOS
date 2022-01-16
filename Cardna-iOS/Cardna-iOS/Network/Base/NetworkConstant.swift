aabdf2b11dcc8ebe955fc82b286776df8aee6539
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-16 23:00:17 +0900

    [#49] 서버통신 상수 추가

--- Cardna-iOS/Cardna-iOS/Network/Base/NetworkConstant.swift
+++ Cardna-iOS/Cardna-iOS/Network/Base/NetworkConstant.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct NetworkConstant {
+    
+    static let noTokenHeader = ["Content-Type": "application/json"]
+    static let hasTokenHeader = ["Content-Type": "application/json",
+                                 "token": NetworkConstant.accessToken]
+    
+    static let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJqaXdvb0BnbWFpbC5jb20iLCJuYW1lIjoi7KeA7JqwIiwiZmlyZWJhc2VJZCI6ImVpSk9qSHJMTWtVOEpiRTg4TUQxWGp6UWhYRzIiLCJpYXQiOjE2NDIzNDExNzEsImV4cCI6MTY0MjQyNzU3MSwiaXNzIjoiY2FyZG5hIn0.6kSJPB7TwdqDsdPIY0hA9CYtUijq-9VcEfCPtzMjelQ"
+}
--- Cardna-iOS/Cardna-iOS/Network/Base/URLConstant.swift
+++ Cardna-iOS/Cardna-iOS/Network/Base/URLConstant.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+struct URLConstant {
+    
+    // MARK: - base URL
+    
+    static let baseURL = "https://asia-northeast3-cardna-29f5b.cloudfunctions.net/api"
+    
+    // MARK: - Auth
+    
+    static let authSignUpEmail = "/auth/signup/email"
+    static let authLoginEmail = "/auth/login/email"
+    static let authRefreshToken = "/auth/refreshtoken"
+    
+    // MARK: - Card
+    
+    static let cardMain = "/card/main"
+    static let card = "/card"
+    static let cardMe = "/card/me"
+    static let cardYou = "/card/you"
+    static let cardInfo = "/card/info"
+    static let cardBox = "/card/box"
+    static let cardPack = "/card/pack"
+    
+    // MARK: - Like
+    
+    static let like = "/like"
+
+    // MARK: - Friend
+    
+    static let friend = "/friend"
+    static let friendList = "/friend/list"
+    static let friendSearch = "/friend/search"
+    
+    // MARK: - MyPAge
+    static let myPage = "/my-page"
+    static let myPageUser = "/my-page/user"
+}
