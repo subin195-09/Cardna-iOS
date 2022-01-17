7c049be2d26b83356113bb1f2f6504072a8d0844
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-01-17 22:27:47 +0900

    [#77] 파일 추가

---
+++ Cardna-iOS/Cardna-iOS/Network/APIModels/EmailSearchModel/EmailSearchResponse.swift
@@ -0,0 +1,8 @@
+//
+//  EmailSearchResponse.swift
+//  Cardna-iOS
+//
+//  Created by 이경민 on 2022/01/17.
+//
+
+import Foundation
---
+++ Cardna-iOS/Cardna-iOS/Network/APIServices/EmailSearchAPI/EmailSearchService.swift
@@ -0,0 +1,8 @@
+//
+//  EmailSearchService.swift
+//  Cardna-iOS
+//
+//  Created by 이경민 on 2022/01/17.
+//
+
+import Foundation
