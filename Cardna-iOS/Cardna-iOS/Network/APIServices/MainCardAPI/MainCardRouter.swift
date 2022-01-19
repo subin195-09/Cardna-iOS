772e5871d687155f9d1a3a735729308ce21c312c
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-19 13:00:48 +0900

    [#118] 마이페이지 -> 친구의 대표카드 화면전환 및 서버연결

--- Cardna-iOS/Cardna-iOS/Network/APIServices/MainCardAPI/MainCardRouter.swift
+++ Cardna-iOS/Cardna-iOS/Network/APIServices/MainCardAPI/MainCardRouter.swift
@@ -10,27 +10,35 @@ import Foundation
 import Moya
 
 enum MainCardRouter {
-    case getMainCard
+    case getMainCard(friendID: Int? = nil)
     case putMainCard(cardIndexList: [Int])
 }
 
 extension MainCardRouter: BaseTargetType {
     var path: String {
         switch self {
-        case .getMainCard, .putMainCard(_): return URLConstant.cardMain
+        case .getMainCard(let friendID):
+            if friendID == nil {
+                return URLConstant.cardMain
+            }
+            else {
+                guard let friendID = friendID else { return URLConstant.cardMain }
+                return "\(URLConstant.cardMain)/\(friendID)"
+            }
+        case .putMainCard(_): return URLConstant.cardMain
         }
     }
     
     var method: Moya.Method {
         switch self {
-        case .getMainCard: return .get
+        case .getMainCard(_): return .get
         case .putMainCard(_): return .put
         }
     }
     
     var task: Task {
         switch self {
-        case .getMainCard:
+        case .getMainCard(_):
             return .requestPlain
         case .putMainCard(let indexList):
             return .requestParameters(parameters: ["cards": indexList], encoding: JSONEncoding.default)
