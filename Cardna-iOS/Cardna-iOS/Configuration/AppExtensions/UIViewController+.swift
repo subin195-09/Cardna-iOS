adb71dc46430508844e89cb961ab5d8cd931d4bc
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-20 00:11:50 +0900

    [#161] 카드삭제 구현

--- Cardna-iOS/Cardna-iOS/Configuration/AppExtensions/UIViewController+.swift
+++ Cardna-iOS/Cardna-iOS/Configuration/AppExtensions/UIViewController+.swift
@@ -9,15 +9,40 @@ import UIKit
 
 extension UIViewController {
     func showToast(message : String, font: UIFont) {
-            let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
-            toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-            toastLabel.textColor = UIColor.white
-            toastLabel.font = font
-            toastLabel.textAlignment = .center;
-            toastLabel.text = message
-            toastLabel.alpha = 1.0
-            toastLabel.layer.cornerRadius = 15;
-            toastLabel.clipsToBounds = true
-            self.view.addSubview(toastLabel)
-            UIView.animate(withDuration: 3.0, delay: 0.2, options: .curveEaseOut, animations: { toastLabel.alpha = 0.0 }, completion: {(isCompleted) in toastLabel.removeFromSuperview() }) }
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 15;
+        toastLabel.clipsToBounds = true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 3.0, delay: 0.2, options: .curveEaseOut, animations: { toastLabel.alpha = 0.0 }, completion: {(isCompleted) in toastLabel.removeFromSuperview() }) }
+    
+    // MARK: 취소와 확인이 뜨는 UIAlertController
+    func makeRequestAlert(title : String,
+                          message : String,
+                          okAction : ((UIAlertAction) -> Void)?,
+                          cancelAction : ((UIAlertAction) -> Void)? = nil,
+                          completion : (() -> Void)? = nil)
+    {
+        
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: cancelAction)
+        alertViewController.addAction(cancelAction)
+        
+        
+        self.present(alertViewController, animated: true, completion: completion)
+    }
 }
