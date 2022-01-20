85b0ef32845b20ffc573f8c6d8a687b53817b97f
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-01-21 02:17:25 +0900

    [#212] 로그아웃 하고 다시 이니셜뷰로 돌아가기

---
+++ Cardna-iOS/Cardna-iOS/Source/Scene/Initial/InitialNavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InitialNavigationViewController.swift
+//  Cardna-iOS
+//
+//  Created by 이경민 on 2022/01/21.
+//
+
+import UIKit
+
+class InitialNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
