54c49cfead67fa484ccf9aaeda822e198bc70f9a
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-01-22 03:58:41 +0900

    [#278] 초기 데이터 비움

--- Cardna-iOS/Cardna-iOS/Source/Scene/CardDetail/CardDetailViewController.swift
+++ Cardna-iOS/Cardna-iOS/Source/Scene/CardDetail/CardDetailViewController.swift
@@ -306,10 +306,10 @@ class CardDetailViewController: UIViewController {
     
     /// 이후 삭제할 더미데이터
     func setDummy() {
-        titleLabel.text = "힘이 들 땐 하늘을 봐.."
-        fromLabel.text = "From. 솝트 디요미 · 혜원"
-        contentLabel.text = "지금 이 말이 우리가 다시 시작하자는 건 아냐 그저 너의 남아있던 기억들이 떠올랐을 뿐이야 정말 하루도 빠짐없이 너는 사랑한다 말해줬었지 잠들기 전에 또 눈 뜨자마자 말해주던 너 생각이 나 말해보는 거야 예뻤어 날 바라봐 주던 그 눈빛 날 불러주던 그 목소리 다 다 그 모든 게 내겐 예뻤어 더 바랄 게 없는듯한 느낌 오직 너만이 주던 순간들 그 모든게 내겐"
-        dateLabel.text = "2022/01/08"
+        titleLabel.text = " "
+        fromLabel.text = " "
+        contentLabel.text = " "
+        dateLabel.text = " "
     }
     
     // MARK: - Objc Function
