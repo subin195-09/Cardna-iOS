232594e86ca6f10128b760546bca5ec9759b3bfc
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-01-22 05:49:45 +0900

    [#274] dark mode 막기

--- Cardna-iOS/Cardna-iOS/Source/Scene/Card/Cell/CardContainerCollectionViewCell.swift
+++ Cardna-iOS/Cardna-iOS/Source/Scene/Card/Cell/CardContainerCollectionViewCell.swift
@@ -16,6 +16,8 @@ class CardContainerCollectionViewCell: UICollectionViewCell {
     var deviceWidth = UIScreen.main.bounds.width
     var isCardMe: Bool = true
     var delegate: CardPassProtocol?
+    var isCardMeEmpty: Bool = true
+    var isCardYouEmpty: Bool = true
     
     // MARK: - IBOutlet
     
@@ -54,47 +56,120 @@ extension CardContainerCollectionViewCell: UICollectionViewDelegate {
 
 extension CardContainerCollectionViewCell: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
         if isCardMe {
-            return CardViewController.cardMeList?.count ?? 0
+            if let cardMe = CardViewController.cardMeList {
+                if cardMe.count > 0 {
+                    isCardMeEmpty = false
+                }
+                else {
+                    isCardMeEmpty = true
+                }
+            }
+            else {
+                isCardMeEmpty = true
+            }
+            return CardViewController.cardMeList?.count ?? 1
         }
         else {
-            return CardViewController.cardYouList?.count ?? 0
+            if let cardYou = CardViewController.cardYouList {
+                if cardYou.count > 0 {
+                    isCardYouEmpty = false
+                }
+                else {
+                    isCardYouEmpty = true
+                }
+            }
+            else {
+                isCardYouEmpty = true
+            }
+            return CardViewController.cardYouList?.count ?? 1
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if isCardMe {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.identifier, for: indexPath) as? CardCollectionViewCell else { return UICollectionViewCell() }
-            cell.setData(image: CardViewController.cardMeList?[indexPath.item].cardImg ?? "", title: CardViewController.cardMeList?[indexPath.item].title ?? "", isMe: true, small: true)
-            return cell
+
+            if isCardMe {
+                if isCardMeEmpty {
+                    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardEmptyCollectionViewCell.identifier, for: indexPath) as? CardEmptyCollectionViewCell else { return UICollectionViewCell() }
+                        return cell
+                }
+                else {
+                    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.identifier, for: indexPath) as? CardCollectionViewCell else { return UICollectionViewCell() }
+                    cell.setData(image: CardViewController.cardMeList?[indexPath.item].cardImg ?? "", title: CardViewController.cardMeList?[indexPath.item].title ?? "", isMe: true, small: true)
+                    return cell
+                }
+            }
+            else {
+                if isCardYouEmpty {
+                    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardEmptyCollectionViewCell.identifier, for: indexPath) as? CardEmptyCollectionViewCell else { return UICollectionViewCell() }
+                        return cell}
+                else {
+                    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.identifier, for: indexPath) as? CardCollectionViewCell else { return UICollectionViewCell() }
+                    cell.setData(image: CardViewController.cardYouList?[indexPath.item].cardImg ?? "", title: CardViewController.cardYouList?[indexPath.item].title ?? "", isMe: false, small: true)
+                    return cell}
+            }
         }
-        else {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.identifier, for: indexPath) as? CardCollectionViewCell else { return UICollectionViewCell() }
-            cell.setData(image: CardViewController.cardYouList?[indexPath.item].cardImg ?? "", title: CardViewController.cardYouList?[indexPath.item].title ?? "", isMe: false, small: true)
-            return cell
-        }
-    }
+    
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if isCardMe {
-            self.delegate?.pushDetailCard(cardID: CardViewController.cardMeList?[indexPath.row].id ?? 0, isMe: 0, isOthers: CardViewController.judgeWhom)
+            if !isCardMeEmpty {
+                self.delegate?.pushDetailCard(cardID: CardViewController.cardMeList?[indexPath.row].id ?? 0, isMe: 0, isOthers: CardViewController.judgeWhom)
+            }
         }
         else {
-            self.delegate?.pushDetailCard(cardID: CardViewController.cardYouList?[indexPath.row].id ?? 0, isMe: 1, isOthers: CardViewController.judgeWhom)
+            if !isCardYouEmpty {
+                self.delegate?.pushDetailCard(cardID: CardViewController.cardYouList?[indexPath.row].id ?? 0, isMe: 1, isOthers: CardViewController.judgeWhom)
+            }
         }
     }
 }
 
 extension CardContainerCollectionViewCell: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellWidth = (deviceWidth-43)/2
-        let cellHeight = cellWidth * (25/16)
-        return CGSize(width: cellWidth, height: cellHeight)
+        if isCardMe {
+            if isCardMeEmpty {
+                let cellWidth = (deviceWidth-50)
+                let cellHeight = cellWidth * (4/3)
+                return CGSize(width: cellWidth, height: cellHeight)
+            }
+            else {
+                let cellWidth = (deviceWidth-43)/2
+                let cellHeight = cellWidth * (25/16)
+                return CGSize(width: cellWidth, height: cellHeight)
+            }
+        }
+        else {
+            if isCardYouEmpty {
+                let cellWidth = (deviceWidth-50)
+                let cellHeight = cellWidth * (4/3)
+                return CGSize(width: cellWidth, height: cellHeight)
+            }
+            else {
+                let cellWidth = (deviceWidth-43)/2
+                let cellHeight = cellWidth * (25/16)
+                return CGSize(width: cellWidth, height: cellHeight)
+            }
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 14, left: 16, bottom: 14, right: 16)
+        if isCardMe {
+            if isCardMeEmpty {
+                return UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+            }
+            else {
+                return UIEdgeInsets(top: 14, left: 16, bottom: 14, right: 16)
+            }
+        }
+        else {
+            if isCardYouEmpty {
+                return UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+            }
+            else {
+                return UIEdgeInsets(top: 14, left: 16, bottom: 14, right: 16)
+            }
+        }
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 12
